8b22cf10cd562b00e46677e5debdef3b43f1ebd7
Natalie C <cyuubiapps@gmail.com>
2020-12-12 14:06:28 -0500

    Change _LOCAL to _DEV

--- Aurora.Runtime/build.h
+++ Aurora.Runtime/build.h
@@ -1,14 +1,14 @@
 #pragma once
 
-#ifdef _LOCAL
-#define BUILD_ID "Local"
+#ifdef _DEV
+#define BUILD_ID "Dev"
 #elif _PROD
 #define BUILD_ID "Prod"
 #else
 #define BUILD_ID "Unknown"
 #endif
 
-#ifdef _LOCAL
+#ifdef _DEV
 #define HOST_URL "http://localhost"
 #else
 #define HOST_URL "https://aurorafn.dev"
